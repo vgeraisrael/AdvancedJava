fda7bec34c388a57ec5942a3b0d0dfcd6fa20a85
Valerii Gerasymovych <vgeraisrael@gmail.com>
2022-01-28 11:32:07 +0200

    Added lessons 7 and 8

---
+++ 7.Condition/JS/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let a = 28;
+if(a == 5) {
+    console.log("Ok");
+} else {
+    console.log("False");
+}
+
+if(a < 5) {
+    console.log("Too low");
+} else if(a > 5) {
+    console.log("Too high");
+} else {
+    console.log("exactly");
+}
+
+a == 6? console.log("Ok") : console.log("False");
+
+switch (a) {
+    case 4:
+        console.log("Less by 1");
+        break;
+    case 6:
+        console.log("More by 1");
+        break;
+    case 5:
+        console.log("Exact");
+        break;
+    default:
+        console.log("not even near");
+}
---
+++ 8.Loops/JS/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let num = 50;
+
+while (num < 55) {
+    console.log(num);
+    num++;
+}
